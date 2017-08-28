d292ff071034366ebf15ceedf88e710e07797f68
robertohonda <roberto_junior_honda@hotmail.com>
2017-08-27 21:43:29 -0400

    Step 3 - Criando e buscando coisas na collection

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
+import '/imports/api/tasks.js';
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+   // code to run on server at startup
 });
